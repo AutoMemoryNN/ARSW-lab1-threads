7822c5cbfbe2b89f79ae3a7a56b64d92e985e4c3
KenjiroYamato <juanpablocamargo91@gmail.com>
2025-01-29 19:50:10 -0500

    feat: countThread implementation

--- src/main/java/edu/eci/arsw/threads/CountThread.java
+++ src/main/java/edu/eci/arsw/threads/CountThread.java
@@ -1,14 +1,35 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package edu.eci.arsw.threads;
 
-/**
- *
- * @author hcadavid
- */
-public class CountThread {
-    
+public class CountThread implements Runnable {
+
+    private final int start;
+    private final int end;
+    private Thread thread;
+
+    public CountThread(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    public void start() {
+        if (thread == null) {
+            thread = new Thread(this);
+            thread.start();
+        }
+    }
+
+    @Override
+    public void run() {
+        for (int i = start; i <= end; i++) {
+            System.out.println(i);
+        }
+    }
+
+    public void interrupt() {
+        if (thread != null && thread.isAlive()) {
+            thread.interrupt();
+            System.out.println("Thread interrupted.");
+        }
+    }
+
 }
--- src/main/java/edu/eci/arsw/threads/CountThreadsMain.java
+++ src/main/java/edu/eci/arsw/threads/CountThreadsMain.java
@@ -6,13 +6,30 @@
 package edu.eci.arsw.threads;
 
 /**
- *
  * @author hcadavid
  */
 public class CountThreadsMain {
-    
-    public static void main(String a[]){
-        
+
+    public static void main(String[] a) {
+        CountThread t1 = new CountThread(0, 99);
+        CountThread t2 = new CountThread(100, 199);
+        CountThread t3 = new CountThread(200, 299);
+
+        System.out.println("Ejecutando con start():");
+        t1.start();
+        t2.start();
+        t3.start();
+
+        try {
+            Thread.sleep(1000); // wait to avoid overlap
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("\n run()-------------------------");
+        t1.run();
+        t2.run();
+        t3.run();
     }
-    
+
 }
